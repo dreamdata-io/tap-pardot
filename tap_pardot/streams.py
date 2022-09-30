64a9d7256621eccd55fd9ba381efe42e6d99de1f
Simon Mikkelsen <simon.mikkelsen@dreamdata.io>
2022-09-30 14:22:06 +0200

    Revert "Revert "Revert "Merge pull request #22 from dreamdata-io/de-1441-pardot-return-exit-5"""
    This reverts commit 2075e9f89700cf99643b5f617bf5406b1ea316c5.

--- tap_pardot/streams.py
+++ tap_pardot/streams.py
@@ -3,8 +3,6 @@ import traceback
 import singer
 import sys
 
-from tap_pardot.client import InvalidCredentials
-
 LOGGER = singer.get_logger()
 
 
@@ -108,15 +106,6 @@ class Stream:
                 for rec in self.sync_page():
                     records_synced += 1
                     yield rec
-
-        except InvalidCredentials as e:
-            LOGGER.error(
-                "exception: %s \n traceback: %s",
-                e,
-                traceback.format_exc(),
-            )
-            sys.exit(5)
-
         except Exception as exc:
             LOGGER.error(
                 "exception: %s \n traceback: %s",
