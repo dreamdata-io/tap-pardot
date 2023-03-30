ff4c32e21543999f05ec8c0d1c4783532b8b0df2
JingLin0 <jinglin.daisy@gmail.com>
2023-03-30 13:30:40 +0200

    sync properties before syncing records

--- tap_pardot/sync.py
+++ tap_pardot/sync.py
@@ -1,6 +1,8 @@
 import singer
 
 from .streams import STREAM_OBJECTS
+from .client import ENDPOINT_BASE, Client
+from typing import Dict, List
 
 LOGGER = singer.get_logger()
 
@@ -16,3 +18,37 @@ def sync(client, config, state):
 
         for rec in stream_object.sync():
             singer.write_record(stream_id, rec)
+
+
+def sync_properties(client: Client):
+    streams = [
+        {
+            "stream": "prospectAccountFields",
+            "path": "prospectAccount/version/4/do/describe",
+            "data_path": ["result", "field"],
+        },
+        {
+            "stream": "customFields",
+            "path": "customField/version/4/do/query",
+            "data_path": ["result", "customField"],
+        },
+    ]
+    for stream in streams:
+        path = stream["path"]
+        data = client._make_request(
+            "GET",
+            f"{ENDPOINT_BASE}{path}",
+            params={"format": "json"},
+        )
+        records = get_data(data, stream["data_path"])
+        singer.write_records(stream["stream"], records)
+
+
+def get_data(data: Dict, path: List) -> Dict:
+    if (not path) or (not data):
+        return data
+    for key in path:
+        data = data.get(key, {})
+        if not data:
+            break
+    return data
